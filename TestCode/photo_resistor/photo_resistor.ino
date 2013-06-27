48a483af68d3fb9dbe19b6b339b4dcf68a6b99c6
Anders Arnholm <Anders.Arnholm@mecel.se>
2013-06-27 13:44:48 +0200

    Add a simple design to test photoresitors

---
+++ TestCode/photo_resistor/photo_resistor.ino
@@ -0,0 +1,23 @@
+int lightPin = 0;
+int ledPin = 3;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode( ledPin, OUTPUT);
+}
+
+void loop()
+{
+  int light = analogRead(lightPin);
+  int out = 256 - ((light - 100)/2);
+  if(out < 0) {
+    out = 0;
+  }
+  if(out > 255) {
+    out = 255;
+  }
+  Serial.print(light); Serial.print(" "); Serial.println(ledPin);
+  analogWrite(ledPin, 256 - light/4);
+  delay(10);
+}
