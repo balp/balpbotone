9c35f2563da2071ff17f417bc7c0d2e17cc610aa
Anders Arnholm <Anders@Arnholm.se>
2013-06-29 11:38:18 +0200

    Setup so we can build from commandline.

---
+++ TestCode/blink.cpp
@@ -0,0 +1,50 @@
+// Blink i C++
+
+#include "Arduino.h"
+
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(100);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(100);               // wait for a second
+}
+
+
+#include <Arduino.h>
+
+int main(void)
+{
+        init();
+
+#if defined(USBCON)
+        USBDevice.attach();
+#endif
+        
+        setup(); // Run my setup function.
+    
+        for (;;) { // Main buzy loop
+                loop();
+                if (serialEventRun) serialEventRun();
+        }
+        
+        return 0;
+}
+
