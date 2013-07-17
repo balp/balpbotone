5980bc15fb5425ab4efdc7c1b0f0fce62d1a8378
Anders Arnholm <Anders@Arnholm.se>
2013-07-17 08:44:23 +0200

    Add some documentations of state

--- Software/state.h
+++ Software/state.h
@@ -1,13 +1,29 @@
-/*
- *
- */
 
+
+
+/**
+ * Keep track of a state.
+ */
 class State {
     public:
+        /**
+         * Interface that is called when the state is changed.
+         */
         class CallbackInterface {
             public:
+                /**
+                 * Virtual DTOR
+                 */
                 virtual ~CallbackInterface() {}
+
+                /**
+                 * Called when the state is entered.
+                 */
                 virtual void onEntry() = 0;
+
+                /**
+                 * Called when the state is exited
+                 */
                 virtual void onExit() = 0;
         };
         State(CallbackInterface* cb) : m_cb(cb) {}
@@ -21,12 +37,26 @@ class State {
         CallbackInterface* m_cb;
 };
 
+/**
+ * A state machine that handles transitions beteen States.
+ */
 class StateMachine {
     public:
+        /**
+         * Constructor this sets the start state to initialState.
+         *
+         * @param initialState The start state for the machine.
+         */
         explicit StateMachine(State& initialState)
             : m_currentState(initialState) {
             initialState.enter();
         }
+
+        /**
+         * Change tghe state of the machine.
+         *
+         * @param state The state t enter.
+         */
         void setState(State& state) {
             m_currentState.exit();
             m_currentState = state;
